86b01bbc5cd2e9f2b59808e9afa3847062316f8c
Mike Bourgeous <mike@mikebourgeous.com>
2016-03-07 01:26:10 -0700

    Added a new program using our graphics primitives to draw a circle.
    The circle has enormous gaps, so more work is needed.

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 .PHONY: all
 
-all: rand
+all: rand circle
 
-rand: rand.c gfx.c
+rand: rand.c gfx.c Makefile
 	colorgcc -Wall -Wextra -O2 rand.c gfx.c -o rand
+
+circle: circle.c gfx.c Makefile
+	colorgcc -Wall -Wextra -O2 circle.c gfx.c -o circle -lm
---
+++ circle.c
@@ -0,0 +1,75 @@
+/*
+ * Uses teaching gfx primitives to draw a circle/ellipse.
+ * Mar. 7, 2016
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+#include "gfx.h"
+
+void circle(uint8_t *img, int w, int h)
+{
+	float r = w < h ? w * 0.5f : h * 0.5f;
+	float xc = 0.5f * w;
+	float yc = 0.5f * h;
+	float t;
+
+	for(t = 0; t <= 2.0f * M_PI; t += 0.1 * M_PI) {
+		int x = r * cosf(t) + xc;
+		int y = r * sinf(t) + yc;
+
+		fprintf(stderr, "t=%.3f x=%d y=%d\n", t, x, y);
+
+		setpx(img, w, x, y, rand() % 127 + 128);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	struct timespec now;
+	uint8_t *img;
+	int w, h;
+	int sz;
+
+	if(argc != 3) {
+		fprintf(stderr, "Usage: %s width height\n", argv[0]);
+		return 1;
+	}
+
+	// Seed the random number generator to get different results each time
+	clock_gettime(CLOCK_MONOTONIC, &now);
+	srand((int)now.tv_sec ^ (int)now.tv_nsec);
+
+	w = atoi(argv[1]);
+	h = atoi(argv[2]);
+	sz = size(w, h);
+
+	fprintf(stderr, "Image size: %dx%d  Array size: %d\n", w, h, sz);
+
+	img = malloc(sz);
+	if(img == NULL) {
+		perror("Failed to allocate image");
+		return 2;
+	}
+
+	memset(img, 0, sz);
+
+	fprintf(stderr, "Allocated image at %p\n", img);
+
+
+	fprintf(stderr, "Drawing a circle or ellipse\n");
+	circle(img, w, h);
+
+	fprintf(stderr, "Writing image to stdout\n");
+	fwrite(img, sz, 1, stdout);
+
+	free(img);
+
+	fprintf(stderr, "Goodbye.\n");
+
+	return 0;
+}
